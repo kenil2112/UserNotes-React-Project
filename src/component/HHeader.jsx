455a56519d0d2173be47584d1c5f80201df97049
Kenil <kenilnakrani202@gmail.com>
2024-03-07 14:56:33 +0530

    original website

--- src/component/HHeader.jsx
+++ src/component/HHeader.jsx
@@ -2,19 +2,12 @@ import React from 'react'
 import { Layout } from 'antd';
 const { Header } = Layout;
 import '../App.css'
-import { Bounce, Flip, ToastContainer, Zoom, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 function HHeader() {
-    const notify = () => toast("Wow so easy!");
-
     return (
         <Header className='HHEader'>
-            <div>
-                <button onClick={notify}>Notify!</button>
-                <ToastContainer position="top-center" theme='pink' transition={Flip} autoClose={500} />
-            </div>
+            Header
         </Header>
     )
 }
